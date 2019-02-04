abe084c0a0b92ea9263024130318dfb1a10eb85f
Peter Freeze <>
2019-02-04 13:14:12 -0600

    Spring cleaning of repository and JS format

--- query-string.js
+++ query-string.js
@@ -1,78 +1,81 @@
-/* QueryString
- * https://github.com/pjfreeze/query-string
- *
- * This is free and unencumbered software released into the public domain.
- */
 (function (global) {
   'use strict';
 
   /**
-   * A simple interface for working with query strings; encoding data to a query string, and
-   * parsing existing query strings into objects.
+   * A simple interface for working with query strings; encoding data to a query
+   * string, and parsing existing query strings into objects.
    */
   class QueryString {
     /**
-     * Parse the provided URL's query string into an object of keys & values. Keys with multiple
-     * values are converted to an array.
+     * Parse the provided URL's query string into an object of keys & values.
+     * Keys with multiple values are combined as an array.
+     *
      * @param {string} url - The URL to parse for query string keys/values
-     * @returns {object} The returned object contains properties with decoded keys and values
+     * @param {function} [decode] - The function used to decode keys & values
+     * @return {object} The returned object contains decoded keys and values
      */
-    static parse (url) {
+    static parse (url, options = { decode: decodeURIComponent }) {
       if (typeof url != 'string') {
         throw new TypeError('[QueryString.parse] "url" must be a string');
       }
 
       const queryStringIndex = url.indexOf('?');
       const doesNotHaveQueryString = queryStringIndex == -1;
-      if (doesNotHaveQueryString) return {};
+      if (doesNotHaveQueryString) { return {}; }
 
       const hashIndex = url.includes('#') ? url.indexOf('#') : url.length;
-      const queryString = url.slice(queryStringIndex + 1, hashIndex);
+      const queryString = url.slice((queryStringIndex + 1), hashIndex);
       const encodedPairs = queryString.split('&');
 
-      return encodedPairs.reduce((parameters, pair) => {
+      return encodedPairs.reduce((memo, pair) => {
         const [encodedKey, encodedValue] = pair.split('=');
-        const decodedKey = decodeURIComponent(encodedKey);
-        const decodedValue = decodeURIComponent(encodedValue);
+        const decodedKey = options.decode(encodedKey);
+        const decodedValue = options.decode(encodedValue);
 
-        const hasExistingEntry = Object.prototype.hasOwnProperty.call(parameters, decodedKey);
+        const hasExistingEntry = has(memo, decodedKey);
 
         if (hasExistingEntry) {
-          const existingValue = parameters[decodedKey];
-          const valueList = Array.isArray(existingValue) ? existingValue : [existingValue];
+          const existingValue = memo[decodedKey];
+          const valueList = (
+            Array.isArray(existingValue)
+            ? existingValue
+            : [existingValue]
+          );
           valueList.push(decodedValue);
-          parameters[decodedKey] = valueList;
+          memo[decodedKey] = valueList;
         } else {
-          parameters[decodedKey] = decodedValue;
+          memo[decodedKey] = decodedValue;
         }
 
-        return parameters;
+        return memo;
       }, {});
     }
 
     /**
-     * Converts and encodes an object into a query string. Array values are added as multiple
-     * entries in the query string.
+     * Converts and encodes an object into a query string. Array values are
+     * added as multiple entries in the query string.
+     *
      * @param {object} parameters - The object to convert to a query string
-     * @returns {string} The query string includes the "?" character if there is one or more pairs
+     * @param {function} [encode] - The function used to encode keys & values
+     * @return {string} Result includes a "?" character if there is >= one key
      */
-    static stringify (parameters) {
+    static stringify (parameters, options = { encode: encodeURIComponent }) {
       if (typeof parameters != 'object' && parameters != null) {
         throw new TypeError('[QueryString.stringify] "parameters" must be an object');
       }
 
       const keys = Object.keys(parameters);
-      if (keys.length == 0) return '';
+      if (keys.length == 0) { return '' };
 
       const pairs = keys.map((key) => {
         const value = parameters[key];
-        const encodedKey = encodeURIComponent(key);
+        const encodedKey = options.encode(key);
         let pair;
         if (Array.isArray(value)) {
-          const values = value.map(each => `${encodedKey}=${encodeURIComponent(each)}`);
+          const values = value.map(each => `${encodedKey}=${options.encode(each)}`);
           pair = values.join('&');
         } else {
-          pair = `${encodedKey}=${encodeURIComponent(value)}`;
+          pair = `${encodedKey}=${options.encode(value)}`;
         }
         return pair;
       });
@@ -81,13 +84,24 @@
     }
   }
 
+  /**
+   * @private
+   * Helper shorthand for property checking
+   *
+   * @param {object} object
+   * @param {string} key
+   * @return {boolean}
+   */
+  const has = (obj, key) => Object.prototype.hasOwnProperty.call(obj, key);
+
   if (typeof define == 'function' && define.amd) {
     define(function () { return QueryString; });
   } else if (typeof exports == 'object') {
     if (typeof module == 'object' && typeof module.exports == 'object') {
       exports = module.exports = QueryString;
+    } else {
+      exports.QueryString = QueryString;
     }
-    exports.QueryString = QueryString;
   } else {
     global.QueryString = QueryString;
   }
--- test/query-string.spec.js
+++ test/query-string.spec.js
@@ -1,47 +1,54 @@
+'use strict';
+
 const assert = require('assert');
 
 const QueryString = require('./../query-string.js');
 
+
 describe('QueryString#parse', () => {
-  it('should return an empty object if the URL doesn\'t have a query string', () => {
+  it('should return an empty object if the URL does not have a query string', () => {
     const result = QueryString.parse('http://example.com');
-    assert(Object.keys(result).length == 0);
+    assert.equal(Object.keys(result).length, 0);
   });
 
   it('should trim the hash from the parsed query string if it exists', () => {
     const result = QueryString.parse('http://example.com?key=value#alphabet-soup');
-    assert(Object.keys(result).length == 1);
-    assert(result.key == 'value');
+    assert.equal(Object.keys(result).length, 1);
+    assert.equal(result.key, 'value');
   });
 
   it('should convert query strings with duplicate keys to arrays', () => {
     const result = QueryString.parse('http://example.com?list=a&list=b&list=c');
-    assert(Object.keys(result).length == 1);
+    assert.equal(Object.keys(result).length, 1);
     assert(Array.isArray(result.list));
-    assert(result.list[0] == 'a');
-    assert(result.list[2] == 'c');
-    assert(result.list[2] == 'c');
+    assert.equal(result.list[0], 'a');
+    assert.equal(result.list[1], 'b');
+    assert.equal(result.list[2], 'c');
   });
 });
 
 describe('QueryString#stringify', () => {
   it('should convert arrays to duplicate key entries', () => {
-    const result = QueryString.stringify({ list: ['a', 'b', 'c'] });
+    const parameters = { list: ['a', 'b', 'c'] };
+    const result = QueryString.stringify(parameters);
     assert(result.includes('list=a&list=b&list=c'));
   });
 
   it('should encode both keys and values', () => {
-    const result = QueryString.stringify({ special: 'a:1' });
-    assert(result.includes(`special=${encodeURIComponent('a:1')}`));
+    const parameters = { special: 'a:1' };
+    const result = QueryString.stringify(parameters);
+    assert(result.includes(`special=${encodeURIComponent(parameters.special)}`));
   });
 
   it('should return an empty string if there were no "pairs"', () => {
-    const result = QueryString.stringify({});
-    assert(result == '');
+    const parameters = {};
+    const result = QueryString.stringify(parameters);
+    assert.equal(result, '');
   });
 
-  it('should return a string with a "?" if there were any "pairs"', () => {
-    const result = QueryString.stringify({ foo: 'bar' });
+  it('should return a string with a "?" if there was at least one key', () => {
+    const parameters = { foo: 'bar' };
+    const result = QueryString.stringify(parameters);
     assert(result.startsWith('?'));
   });
 });
